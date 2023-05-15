1c50ba87fc7f946f5dd6d0f2cbeeef622e30d269
rana-abusamhan <rana.abusamhan@gmail.com>
2023-05-15 12:54:48 +0000

    sds

---
+++ insertSchema.sql
@@ -0,0 +1,3 @@
+INSERT INTO favMemes (id, bottomText, image_path, meme_name, rank, tags, topText)
+VALUES (2, 'bad', 'https://imgflip.com/s/meme/Brace-Yourselves-X-is-Coming.jpg', 'Grumpy Cat',
+'10', 'Tardar Sauce, Tabatha Bundesen, Felis domesticus' , '');
